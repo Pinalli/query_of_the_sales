849cfb11f5524b28b1da5ba00dc8a4f4d50c5f1e
Alberto Rocha Pinalli <betopinalli@gmail.com>
2022-07-11 12:15:08 -0300

    Project created

---
+++ backend/src/main/java/com/pinalli/query_sales/QuerySalesApplication.java
@@ -0,0 +1,13 @@
+package com.pinalli.query_sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerySalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerySalesApplication.class, args);
+	}
+
+}
