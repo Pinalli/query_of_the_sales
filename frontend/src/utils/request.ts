50a2e6a8947df82a102ccdeafae368545cdc68de
Alberto Rocha Pinalli <betopinalli@gmail.com>
2022-07-15 13:48:59 -0300

    Sale listing

---
+++ frontend/src/utils/request.ts
@@ -0,0 +1,5 @@
+
+
+                      // variaval de ambiente     if not exist         else      
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
+                                                   //  coalescência
